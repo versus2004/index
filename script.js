1b1fe252e5edb35c86c5cf3e5c44598ce137a69e
versus2004 <132815243+versus2004@users.noreply.github.com>
2023-06-05 17:09:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleDescription(descriptionId) {
+    const description = document.getElementById(descriptionId);
+    description.style.display = description.style.display === 'none' ? 'block' : 'none';
+}
